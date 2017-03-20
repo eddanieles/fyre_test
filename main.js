eb3fbc4ad5f0f34e52192cd4a7a764b2592500bf
eddanieles <ed.danieles@yahoo.com>
2017-03-20 19:53:39 -0400

    displaying POST response to user

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
             active: res.data.status,
             currentSalary: '',
             desiredSalary: '',
-            willRelocate: res.data.profile.willingToRelocate,
+            willRelocate: (res.data.profile.willingToRelocate === true),
             externalId: res.data.id.toString(),
             personal: {
               first: res.data.firstName,
@@ -41,9 +41,9 @@ $(document).ready(function() {
               address: res.data.addresses[0].line1,
               address2: res.data.addresses[0].line2,
               city: res.data.addresses[0].city,
-              state: res.data.addresses[0].state,
-              zip: res.data.addresses[0].zip,
-              countryCode: res.data.addresses[0].country
+              state: res.data.addresses[0].state.toUpperCase(),
+              zip: res.data.addresses[0].zip.toString(),
+              countryCode: res.data.addresses[0].country.substring(0, 2)
             },
             contactMethods: {
               email: res.data.contactMethods[1].text
@@ -52,14 +52,22 @@ $(document).ready(function() {
           //console.log(userObj);
         }
     }).then(function() {
-      console.log(userObj);
+      //console.log(userObj);
       $.ajax({
           url: `https://fyre-tech-test.herokuapp.com/candidate?auth=${API_KEY}`,
           type: 'POST',
           contentType: 'application/json; charset=UTF-8',
           data: JSON.stringify(userObj),
           success: function(res) {
-            console.log(res);
+            console.log(res.data);
+            $('.candidate_list').prepend(`
+              <li>
+              <p>ID: ${res.data.externalId}</p>
+              <p>Name: ${res.data.personal.first} ${res.data.personal.last}</p>
+              <p>Address: ${res.data.homeAddress.address} ${res.data.homeAddress.city}, ${res.data.homeAddress.state} ${res.data.homeAddress.zip}</p>
+              <p>Email: ${res.data.contactMethods.email}</p>
+              </li>
+              `);
           }
       });
     })
